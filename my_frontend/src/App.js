8b2edbb287987cb6572994c395d71a4343f67b62
Kisekka-Alexander <53447358+Kisekka-Alexander@users.noreply.github.com>
2022-05-31 16:54:44 +0300

    Update App.js

--- my_frontend/src/App.js
+++ my_frontend/src/App.js
@@ -1,21 +1,26 @@
 import { BrowserRouter as Router, Route,Routes} from 'react-router-dom'
 import Sidebar from './components/Sidebar'
 import Tasks from './components/Tasks'
+import './App.css'
 
 function App() {
   return (
-    <Router>
-    <Routes>
+  //   <Router>
+  //   <Routes>
   
-    <Route path='/' element={
-      <div className="App" id="outer-container">
-      <Sidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
-      </div>
-    }/>
+  //   <Route path='/' element={
+  //     <div className="App" id="outer-container">
+  //     <Sidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
+  //     </div>
+  //   }/>
  
-  <Route path='/tasks' element={<Tasks />} />
-  </Routes>
-  </Router>
+  // <Route path='/tasks' element={<Tasks />} />
+  // </Routes>
+  // </Router>
+<div className='App'>
+<Sidebar/>
+</div>
+
   );
 }
 
