6d6fa8802361845ac391789b90084976f813babc
Kisekka-Alexander <53447358+Kisekka-Alexander@users.noreply.github.com>
2022-05-31 16:54:51 +0300

    Create SidebarData.js

---
+++ my_frontend/src/components/SidebarData.js
@@ -0,0 +1,24 @@
+import HomeIcon from '@mui/icons-material/Home';
+
+export const SidebarData = [
+
+    {
+        title: 'Home',
+        icon: <HomeIcon/>,
+        Link: '/home'
+    },
+
+    {
+        title: 'Home',
+        icon: <HomeIcon/>,
+        Link: '/home'
+    },
+
+    {
+        title: 'Home',
+        icon: <HomeIcon/>,
+        Link: '/home'
+    },
+
+]
+
