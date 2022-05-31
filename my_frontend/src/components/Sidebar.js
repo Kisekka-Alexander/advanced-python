a592cc499f461f53ae5fb4027f8341b3d8936335
Kisekka-Alexander <53447358+Kisekka-Alexander@users.noreply.github.com>
2022-05-31 16:54:47 +0300

    Update Sidebar.js

--- my_frontend/src/components/Sidebar.js
+++ my_frontend/src/components/Sidebar.js
@@ -1,19 +1,35 @@
 
-import { slide as Menu } from 'react-burger-menu'
-import { Link } from 'react-router-dom'
+// import { slide as Menu } from 'react-burger-menu'
+// import { Link } from 'react-router-dom'
+import '../App.css'
+import {SidebarData} from './SidebarData'
 
 const Sidebar = () => {
   return (
-    <Menu>
-      <a href="/">
-        Home
-      </a>
-     
-      <Link to='/tasks'>Tasks</Link>
-    </Menu>
+    // <Menu>
+    //   <a href="/">
+    //     Home
+    //   </a>
+    //   <Link to='/tasks'>Tasks</Link>
+    // </Menu>
   
-  
+  <div className='Sidebar'>
+    <ul className='SidebarList'>
+       {SidebarData.map((val,key)=> {
+         return(
+         <li className='row' key={key} onClick={()=>{window.location.pathname=val.Link}}>
+           {" "}
+         <div id='icon'>{val.icon}</div>
+         {" "}
+         <div id='title'>{val.title}</div>
+         </li>
+         )
+         
+       })}
+    </ul>
+  </div>
   )
+  
 }
 
 export default Sidebar
